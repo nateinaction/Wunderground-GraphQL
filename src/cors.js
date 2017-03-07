fa3a8a7eb981e405d529bd70adfc5c79647e376b
Nate Gay <nate@ohnate.com>
2017-03-07 01:28:58 -0600

    lokka GraphQL client needed extra headers, setting Access-Control-Allow-Credentials to true

--- src/cors.js
+++ src/cors.js
@@ -6,6 +6,7 @@ var corsConfig = (req, callback) => {
   var origin = req.header('Origin') || null
   var hostname = (origin) ? url.parse(origin).hostname : null
   var config = (allowedOrigins.indexOf(hostname) !== -1) ? { origin: true } : { origin: false }
+  config.credentials = true
   callback(null, config)
 }
 
